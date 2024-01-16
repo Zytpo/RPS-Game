46779a52b844e64009cd7edac3edd8c65ab33f30
AlanPapic <alan.papic@hotmail.com>
2024-01-16 15:21:18 +0100

    User score increment/reduction
    User score UI and backend handling with increment/reduction functionality

--- Main-Comp/javascript.js
+++ Main-Comp/javascript.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Wait for the DOM to be fully loaded before attaching event listeners
 
     // Get references to the buttons
+    var clientScore = 0
     var rockButton = document.getElementById('main-div-rock-button');
     var paperButton = document.getElementById('main-div-paper-button');
     var scissorButton = document.getElementById('main-div-scissor-button');
 
     function gameEval(clientChoice) {
 
+        var clientStatus //1 = won, 2 = tie, 3 = lost
         var cpuChoice
         var num = Math.floor(Math.random() * 3)
         var text
@@ -16,32 +18,53 @@ document.addEventListener('DOMContentLoaded', function() {
             
             if (clientChoice === "Rock") {
                 text = "Cpu choose rock, the game is a tie"
+                clientStatus = 2
             } else if (clientChoice === "Paper"){
                 text = "Cpu choose rock, you win!"
+                clientStatus = 1
             } else {
                 text = "Cpu choose rock, you lose"
+                clientStatus = 3
             }
 
         } else if (num === 1) { //Cpu choose paper
 
             if (clientChoice === "Rock") {
                 text = "Cpu choose paper, you lose"
+                clientStatus = 3
             } else if (clientChoice === "Paper"){
                 text = "Cpu choose paper, it's a tie"
+                clientStatus = 2
             } else {
                 text = "Cpu choose paper, you win!"
+                clientStatus = 1
             }
             
         } else { //Cpu choose scissor
 
             if (clientChoice === "Rock") {
                 text = "Cpu choose scissor, you win!"
+                clientStatus = 1
             } else if (clientChoice === "Paper"){
                 text = "Cpu choose scissor, you lose"
+                clientStatus = 3
             } else {
                 text = "Cpu choose scissor, it's a tie"
+                clientStatus = 2
             }
             
+        }
+
+        if (clientStatus === 1) {
+            clientScore += 1
+            document.getElementById("main-div-cputab-text").style.color = "Green"
+            document.getElementById("main-div-clienttab-text").innerHTML = "Your score " + clientScore
+        } else if (clientStatus === 2) {
+            document.getElementById("main-div-cputab-text").style.color = "#EC8B49"
+        } else {
+            clientScore -= 1
+            document.getElementById("main-div-cputab-text").style.color = "Red"
+            document.getElementById("main-div-clienttab-text").innerHTML = "Your score " + clientScore
         }
 
         return text
